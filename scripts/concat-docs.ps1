e6fb6143543c9ddc840b094ee901deef19fb60ae
akaghef <mech.pooh@gmail.com>
2026-07-21 08:36:28 +0900

    chore(repo): remove TheDesign generation and consolidate docs

--- scripts/concat-docs.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [Parameter(ValueFromRemainingArguments = $true)]
-  [string[]] $Args
-)
-
-$ErrorActionPreference = "Stop"
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-& node (Join-Path $ScriptDir "concat-docs.mjs") @Args
-exit $LASTEXITCODE
